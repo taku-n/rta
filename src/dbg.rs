1d0f6f9b7733102a25049183d3f371f1de956f57
Takumi Nishijo <hoge@hoge.com>
2019-03-15 01:06:54 +0900

    add generic function

--- src/dbg.rs
+++ src/dbg.rs
@@ -1,7 +1,26 @@
-// The server program is https://github.com/taku-n/srv
+// The server program is
+// https://github.com/taku-n/dbg or https://github.com/taku-n/srv
 
+use std::fmt::Debug;
 use std::io::prelude::*;
 use std::net::TcpStream;
+
+const ADDR_PORT: &str = "127.0.0.1:9841";
+
+pub fn dbg<T>(t: T)
+        where T: Debug {
+    let stream = TcpStream::connect(ADDR_PORT);
+
+    let mut stream = match stream {
+        Ok(t) => t,
+        Err(e) => {
+            return
+        },
+    };
+
+    let msg = format!("{:?}", t);  // Should this be a CString?
+    stream.write(msg.as_bytes());
+}
 
 pub fn str(s: &str) {
     let stream = TcpStream::connect("127.0.0.1:8800");
@@ -30,4 +49,18 @@ pub fn s_i32(s: &[i32]) {
 
 pub fn s_f64(s: &[f64]) {
     str(&format!("{:.6?}", s));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn dbg_test() {
+        dbg("hello, world");
+        dbg(2);
+        dbg(3.0);
+        dbg(&[2, 3, 5]);
+        dbg(&[7.0, 11.0, 13.0]);
+    }
 }
